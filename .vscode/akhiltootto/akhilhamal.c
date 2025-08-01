d30e6333495321d22b023734cbde6b0056ed470c
SUNWALKER469 <akhilhamal2005@gmail.com>
2025-08-01 08:17:15 +0545

    first commit

---
+++ .vscode/akhiltootto/akhilhamal.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main() 
+{
+    int sub, sum, mul, firstNumber, secondNumber;
+    float div;
+
+    printf("Enter first number:");
+    scanf("%d", &firstNumber);
+
+    printf("Enter first number:");
+    scanf("%d", &firstNumber);
+
+    //calculation
+    sum = firstNumber + secondNumber;
+    sub = firstNumber - secondNumber;
+    mul = firstNumber * secondNumber; 
+    div = firstNumber / secondNumber;
+
+    //printing results
+    printf("The sum is:%d", sum);
+    printf("The sum is:%d", sum);
+    printf("The sum is:%d", sum);
+    printf("The sum is:%d", sum);
+
+    getchar();
+    return 0;
+
+}
