13544b0fb14f3e3f49410dbff6289c2c9c0793e2
SUNWALKER469 <akhilhamal2005@gmail.com>
2025-08-05 07:24:16 +0545

    wassup guys

---
+++ akhil.c
@@ -0,0 +1,22 @@
+// WAP to display greatest numbers among three numbers using if-else.
+#include<stdio.h>
+int main() {
+    int a, b, c;
+
+    // Input three numbers
+    printf("Enter three numbers:\n");
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Compare using if-else
+    if (a >= b && a >= c) {
+        printf("The greatest number is: %d\n", a);
+    }
+    else if (b >= a && b >= c) {
+        printf("The greatest number is: %d\n", b);
+    } 
+    else {
+        printf("The greatest number is: %d\n", c);
+    }
+
+    return 0;
+}
