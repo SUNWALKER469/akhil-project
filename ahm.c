1e6b159a0500ed594ae12452e0cda25f7b1c0837
SUNWALKER469 <akhilhamal2005@gmail.com>
2025-08-06 21:03:32 +0545

    you kcuff

---
+++ ahm.c
@@ -0,0 +1,15 @@
+//WAP to find if a number is greater than 9 & less than 100 using AND operation.
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int x;
+	printf("Akhil Hamal\n");
+	printf("Enter a number:");
+	scanf("%d", &x);
+	printf("%d\n", x>9 && x<100);
+	return 0;
+	
+}
