4b5b9e61bd40ec61d1339cee47157b6d215e0c84
SUNWALKER469 <akhilhamal2005@gmail.com>
2025-08-06 06:55:29 +0545

    you kcuf

---
+++ add.c
@@ -0,0 +1,60 @@
+//Matrix Addition 
+#include<stdio.h>
+
+int main()
+{
+    int a[3][3], b[3][3], sum[3][3] ;
+    int rows, cols;
+    
+    printf("Akhil Hamal\n");
+    
+    // Input size of matrix
+    printf("Enter rows and cols of the matrices:");
+    scanf("%d%d", &rows, &cols);
+    
+   
+    
+    //Input elements of the first matrix
+    printf("Enter the elements of the first matrix:\n");
+    for(int i = 0; i<rows; i++)
+    {
+        for(int j = 0; j<cols; j++){
+        
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+     //Input elements of the second matrix
+    printf("Enter the elements of the second matrix:\n");
+    for(int i = 0; i<rows; i++)
+    {
+        for(int j = 0; j<cols; j++){
+        
+            scanf("%d", &b[i][j]);
+        }
+    }
+    
+    
+      
+    // matrix addition
+    for(int i=0; i<rows; i++)
+    {
+        for(int j=0; j<cols; j++)
+        {
+           sum[i][j] = a[i][j] + b[i][j]  ;
+       }
+   }
+   
+    // Display the result
+    printf("The sum of the two matrices is:\n");
+    for(int i=0; i<rows; i++)
+    {
+        for(int j=0; j<cols; j++)
+        {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
