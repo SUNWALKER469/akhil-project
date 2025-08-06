2e7cfcae108a25f53f5f1404eae80b5990da7a97
SUNWALKER469 <akhilhamal2005@gmail.com>
2025-08-06 20:47:49 +0545

    you  kcuf

---
+++ aaaa.c
@@ -0,0 +1,12 @@
+//WAP to find if its monday or if its raining using OR operation
+#include<stdio.h>
+
+int main()
+{
+	int isMonday = 0;
+	int isRaining = 1;
+	printf("Akhil Hamal\n");
+    
+	printf("The result is:%d\n", isMonday || isRaining);
+	return 0;
+}
