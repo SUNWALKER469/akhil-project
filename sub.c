833563dd020eb48df925a4f3fb1a3bc57070eccf
SUNWALKER469 <akhilhamal2005@gmail.com>
2025-08-06 20:41:00 +0545

    you kcuf

--- sub.c
+++ sub.c
@@ -1,60 +1,12 @@
-//Matrix Subtraction
+//WAP to find if its monday or its raining using OR operation
 #include<stdio.h>
+#include<math.h>
 
 int main()
 {
-    int a[3][3], b[3][3], sub[3][3] ;
-    int rows, cols;
-    
-    printf("Akhil Hamal\n");
-    
-    // Input size of matrices
-    printf("Enter rows and cols of the matrices:");
-    scanf("%d%d", &rows, &cols);
-    
-   
-    
-    //Input elements of the first matrix
-    printf("Enter the elements of the first matrix:\n");
-    for(int i = 0; i<rows; i++)
-    {
-        for(int j = 0; j<cols; j++){
-        
-            scanf("%d", &a[i][j]);
-        }
-    }
-    
-     //Input elements of the second matrix
-    printf("Enter the elements of the second matrix:\n");
-    for(int i = 0; i<rows; i++)
-    {
-        for(int j = 0; j<cols; j++){
-        
-            scanf("%d", &b[i][j]);
-        }
-    }
-    
-    
-      
-    // matrix subtraction
-    for(int i=0; i<rows; i++)
-    {
-        for(int j=0; j<cols; j++)
-        {
-           sub[i][j] = a[i][j] - b[i][j]  ;
-       }
-   }
-   
-    // Display the result
-    printf("The subtraction of the two matrices is:\n");
-    for(int i=0; i<rows; i++)
-    {
-        for(int j=0; j<cols; j++)
-        {
-            printf("%d\t", sub[i][j]);
-        }
-        printf("\n");
-    }
-    
-    return 0;
+	int isMonday = 0;
+	int isRaining = 1;
+	printf("Akhil Hamal\n");
+	printf("The result is:%d\n", isMonday || isRaining);
+	return 0;
 }
