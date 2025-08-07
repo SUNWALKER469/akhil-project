25b429e180364019cbe324d40694de236f487f46
SUNWALKER469 <akhilhamal2005@gmail.com>
2025-08-07 06:05:16 +0545

    you kcuf

---
+++ ram.c
@@ -0,0 +1,23 @@
+//Arithmetic operators 
+#include<stdio.h>
+int main(){
+	int x= 40, y=25;
+	int add, mul, div, sub, mod;
+	printf("AKhil Hamal\n");
+	
+	//Calculation
+	add= x + y;
+	sub= x - y;
+	mul= x * y;
+	div= x / y;
+	mod= x % y;
+	
+	//Result
+	printf("Additon of two number is:%d\n", add);
+	printf("Subtraction of two number is:%d\n", sub);
+	printf("Multiplication of two number is:%d\n", mul);
+	printf("Division of two number is:%d\n", div);
+	printf("Modulous of two number is:%d\n", mod);
+	
+	return 0;
+}
