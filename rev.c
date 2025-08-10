d7572af2aefa8b111f97919d13a57a9b4b6b6dc2
SUNWALKER469 <akhilhamal2005@gmail.com>
2025-08-10 10:58:33 +0545

    you kcuf

---
+++ rev.c
@@ -0,0 +1,22 @@
+//WAP to print sum of numbers and reverse them in order.
+#include<stdio.h>
+
+int main() 
+{
+	int n;
+	printf("enter a number:");
+	scanf("%d", &n);
+	
+	int sum = 0;
+	for(int i=1; i<=n; i++) {
+		sum += i;
+	
+	}
+		printf("sum is:%d\n", sum);
+	
+	
+	for(int i=n; i>=1; i--) {
+		printf("%d\n", i);
+	}
+	return 0;
+}
