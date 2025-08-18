b87a2ced2c8fcc11ff4a367d3b966604c79a2098
SUNWALKER469 <akhilhamal2005@gmail.com>
2025-08-18 12:45:32 +0545

    you kcuf

---
+++ vowel_or_not.c
@@ -0,0 +1,15 @@
+// WAP in C which tells user whether the word is vowel or not
+#include <stdio.h>
+int main() {
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+        ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+        printf("%c is a vowel.\n", ch);
+    else
+        printf("%c is not a vowel.\n", ch);
+
+    return 0;
+}
