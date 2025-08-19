876930167380037274fefcf0e856855145d322ac
SUNWALKER469 <akhilhamal2005@gmail.com>
2025-08-19 13:51:19 +0545

    you kcuf

---
+++ size_of_integers.c
@@ -0,0 +1,10 @@
+//WAP to find the size of different integers.
+#include<stdio.h>
+int main()
+{
+	printf("Akhil Hamal\n");
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(char));
+	return 0;	
+}
